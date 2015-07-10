d270bd27da55503ac7a05266f25d7edd473bff17
kentoku24 <kentoku.matsunami@gmail.com>
2015-07-10 21:40:20 +0900

    色々変更
    ※なおまだ動かないもよう

--- splatoonScraper.rb
+++ splatoonScraper.rb
@@ -1,22 +1,42 @@
 require 'time'
-
+require 'yaml'
 #{battle_type:{stage:time}}
+
+e_e = "Windows31J"
+
+#val = ARGF.each_line.each_slice(4).take(4)
+
+File.open("splatoon_text","w:UTF-8:Windows-31J").each.each_slice(4).each do |dates,battle_type,stages,other|
+  p battle_type.encoding
+  p "#{dates} #{battle_type.encode e_e} #{stages.encode e_e}"
+end
+
+print val
+print "-------------------"
 
 hash = {}
 
-#lines = "foo"
+lines = ["roo","bar"]
+count = 0
+total_duration = 0
 
-
-
-ARGF.each do |line|
+lines.each do |line|
   battle_type = "nawabari"
   stage = "mozuku"
   time_from = Time.parse "2013-10-01 23:00"
   time_to   = Time.parse "2013-10-01 24:00"
   duration = time_to - time_from
   hash[battle_type] = {} if hash[battle_type].nil?
-  hash[battle_type][stage] = 0 if hash[battle_type][stage].nil?
-  hash[battle_type][stage] += duration
+  hash[battle_type][stage] = {} if hash[battle_type][stage].nil?
+  hash[battle_type][stage][:total] = 0 if hash[battle_type][stage][:total].nil?
+  hash[battle_type][stage][:time_list] = [] if hash[battle_type][stage][:time_list].nil?
+
+  hash[battle_type][stage][:time_list].push [time_from, time_to]
+  hash[battle_type][stage][:total] += duration
+
+  count += 1
+  total_duration += duration
 end
 
 print hash
+print "average#{total_duration/count}"
